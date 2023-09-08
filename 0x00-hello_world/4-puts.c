e5900d32383cbc5179b8be64e1e1cbf03b776599
NDIKAY <nbarthazar80@gmail.com>
2023-09-08 12:29:07 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- *main - main entry point
- *ruturn : always 0 (success)
+ * main - main entry point
+ * Description: This is function entry
+ * ruturn : always 0 (success)
  */
 int main(void)
 {
