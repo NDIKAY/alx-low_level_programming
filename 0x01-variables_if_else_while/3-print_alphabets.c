a7587ea570a04d76ffc9cbc1954844ba47de10da
NDIKAY <nbarthazar80@gmail.com>
2023-09-11 04:08:04 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int lowerCase = 'a';
 int upperCase = 'A';
-while (lowerCase <='Z')
+while (lowerCase <= 'Z')
 {putchar(lowerCase);
 lowerCase += 1;
 }
@@ -18,6 +18,5 @@ while (upperCase <= 'Z')
 putchar(upperCase);
 upperCase += 1;
 }
-putchar('\n');
 return (0);
 }
