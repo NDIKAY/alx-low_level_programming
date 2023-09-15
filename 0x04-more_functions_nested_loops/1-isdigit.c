eb737674b762613059694dc9614dabb304ed14e6
NDIKAY <nbarthazar80@gmail.com>
2023-09-15 15:31:13 -0700

    isddigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_isdigit - verify for uppercase
+*@c: parameter
+*return: 1 if collect
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
