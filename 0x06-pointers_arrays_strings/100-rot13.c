6648905c01784a74f5acce5ebc1a67532bc61302
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2023-09-22 03:59:01 +0200

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*rot13 - Encoding string
+*@s: parammeter
+*Return: Str
+**/
+
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijklmnopqrstuvwyz";
+	char data2[] = "NOPQRSTUVWYZABCDEFGHIJKLMnopqrstuvwyzabcdefghijklm";
+
+	for (j = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = data2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
+
