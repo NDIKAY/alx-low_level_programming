7279421c1d557b3146c8ef5e209a0f95fb5b4434
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2023-10-06 05:55:26 +0200

    ok

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,103 +5,92 @@
 /**
  *is_digit - enter point
  *@c: parameter
- *Return: c as output
+ *Return: 0 as output
  */
-int is_digit(char c)
+int is_digit(char *s)
 {
-	return (c >= '0' && c <= '9');
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return(1);
 }
 
 /**
- *print_error_and_exit - enter point
+ *_strlen - Function
+ *@s: parameter
+ *Return: length
  */
-
-void print_error_and_exit(void)
-
+int _strlen(char *s)
 {
-	printf("Error\n");
+	int i = 0;
+
+	while (s[i]  != '\0')
+	{
+		i++;
+	}
+	return(i);
+}
+/**
+ * errors - Function
+ */
+void errors(void)
+{
+	printf("error\n");
 	exit(98);
 }
 
 /**
- *multip - Enter point
- *@num1: Parameter
- *@num2: Parameter
- */
-
-void multip(char *num1, char *num2)
+ *main - multiplication
+ *@argc: nuber of srg
+ *@argv: array of arguments
+ *Return: 0
+*/
+int main (int argc, char *argv[])
 {
-	int len1 = 0, len2 = 0;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	while (num1[len1] && is_digit(num1[len1]))
-		len1++;
-	while (num2[len2] && is_digit(num2[len2]))
-
-		len2++;
-
-	if (len1 == 0 || len2 == 0)
-		print_error_and_exit();
-
-	int result_len = len1 + len2;
-
-	int *result = calloc(result_len, sizeof(int));
-
-	if (result == NULL)
-		print_error_and_exit();
-
-	for (int i = len1 - 1; i >= 0; i--)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if(!result)
+		return (1);
+	for (i = 0; i <= len1 +len2; i++)
+		result[i] = 0;
+	for (len = len1 - 1; len1 >= 0; len1--)
 	{
-		for (int j = len2 - 1; j >= 0; j--)
+		digit1 =s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			int mul = (num1[i] - '0') * (num2[j] - '0');
-			int sum = mul + result[i + j + 1];
-
-			result[i + j] += sum / 10;
-			result[i + j] + 1] = sum % 10;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 +len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	int start = 0;
-
-	while (start < result_len - 1 && result[start] == 0)
-		start++;
-	for (int i = start; i < result_len; i++)
-		printf("%d", result[i]);
-
-	printf("\n");
-
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
 	free(result);
-}
-
-/**
-*main - enter point
-*@argc: argument
-*@argv: argument
-*Return: 0 on success
- */
-
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-		print_error_and_exit();
-
-	char *num1 = argv[1];
-	char *num2 = argv[2];
-
-
-	while (*num1)
-	{
-		if (!is_digit(*num1))
-			print_error_and_exit();
-		num1++;
-	}
-	while (*num2)
-	{
-		if (!is_digit(*num2))
-			print_error_and_exit();
-		num2++;
-	}
-	multip(argv[1], argv[2]);
-
 	return (0);
-
 }
+
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,8 +7,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int is_digit(char c);
-void print_error_and_exit();
-void multip(char *num1, char *num2);
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
+
 
 #endif
