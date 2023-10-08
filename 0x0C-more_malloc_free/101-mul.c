8b03cd048f9490c95a493eedd9690a36d0b3858a
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2023-10-08 21:08:03 +0200

    ok

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -66,9 +66,9 @@ int main(int argc, char *argv[])
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len = len1 - 1; len1 >= 0; len1--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 =s1[len1] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
