447ad3a6d58d4ce213b24d8d0d69fc7e13ffcdbb
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2023-10-09 14:39:51 +0200

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure
+ *@name: dog name
+ *@age: dog age
+ *@owner: name of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
+
+#endif
