f0d107e522ed97761c3fa5659854acb812b2f864
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2023-10-24 14:55:00 +0200

    0. Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *print_listint - print the int data in a singly linked list
+ *@h: head pointer to the list
+ *Return: number of node in the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t strcount;
+
+	if (h == NULL)
+		return (0);
+	for (strcount = 0; h != NULL; strcount++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (strcount);
+}
