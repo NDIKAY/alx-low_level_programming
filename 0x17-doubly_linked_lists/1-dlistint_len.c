038b52a987c2aecaa2c05aac023c17e8f49048b6
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2023-12-07 23:26:23 +0200

    0. Print list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements in a double linked list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int node_counter = 0;
+
+	if (h == 0)
+		return (node_counter);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		node_counter++;
+		h = h->next;
+	}
+	return(node_counter);
+}
