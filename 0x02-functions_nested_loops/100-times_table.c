819a9af156420c766792bcd3f3544b16d0e8a87e
NDIKUBWIMANA Balthazar <nbarthazar80@gmail.com>
2025-05-22 05:08:49 +0200

    okok

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,40 +1,60 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-*Print_times_table - Print tables
-*@n: parameter
-*/
-
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: The number range of the times table (0 ≤ n ≤ 15)
+ */
 void print_times_table(int n)
 {
 	int i, j, result;
 
-	if  (n >= 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
 				result = i * j;
-				if (result <= 9)
-					_putchar(' ');
-				if (result <= 99)
-				      	_putchar(' ');
+
+				/* Print comma and spacing for all except first column */
+				if (j != 0)
+				{
+					_putchar(',');
+					if (result < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else if (result < 100)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else
+					{
+						_putchar(' ');
+					}
+				}
+
+				/* Print the number itself */
 				if (result >= 100)
 				{
-					_putchar((result / 100) + 48);
-					_putchar((result / 10) % 10 + 48);
-				} else if (result <= 99 && result >= 10)
-					_putchar((result / 10) + 48);
-				_putchar((result % 10) + 48);
+					_putchar((result / 100) + '0');
+					_putchar((result / 10 % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else if (result >= 10)
+				{
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+				{
+					_putchar(result + '0');
+				}
 			}
 			_putchar('\n');
 		}
-
 	}
 }
-
